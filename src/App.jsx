2b4d828d2f57e55c2dcb7b714c81bc5d60062966
Fco. Molpeceres <fran.mscr@gmail.com>
2026-04-16 20:59:22 +0200

    feat: added wipe button

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,22 @@ const App = () => {
     setText(target.value);
   };
 
+  const handleClick = () => {
+    setText("");
+  };
+
   return (
     <section>
       <h1>Title H1</h1>
 
       <section>
         <h2>Input</h2>
-        <input type="text" onChange={handleChange} />
+        <input type="text" onChange={handleChange} value={text} />
       </section>
 
       <section>
         <h2>Text</h2>
+        {text && <button onClick={handleClick}>Wipe text</button>}
         <p>{text}</p>
       </section>
     </section>
