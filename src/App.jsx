1e23938aa54271068340644531dbf1bae443b1f9
Fco. Molpeceres <fran.mscr@gmail.com>
2026-04-16 20:52:50 +0200

    feat: added text and handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 const App = () => {
+  const [text, setText] = useState("");
+
+  const handleChange = ({ target }) => {
+    setText(target.value);
+  };
+
   return (
     <section>
-      <h1>Title test</h1>
+      <h1>Title H1</h1>
 
       <section>
-        <input type="text" />
+        <h2>Input</h2>
+        <input type="text" onChange={handleChange} />
+      </section>
+
+      <section>
+        <h2>Text</h2>
+        <p>{text}</p>
       </section>
     </section>
   );
